0b79191958b401152db9dc8d983142c8c51b02cd
Michael Prinsloo <mike.prinsloo@gmail.com>
2014-03-25 00:02:48 +0200

    drush command is now working.

--- go-module-check.go
+++ go-module-check.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"os"
 	"log"
 	"path/filepath"
+	"os/exec"
+	"github.com/mgutz/ansi"
+	"strings"
 )
 
 type SiteList struct {
@@ -23,6 +27,8 @@ func inList(item string, list []string) bool {
 
 func main() {
 
+	var e error
+
 	if len(os.Args) < 2 {
 		log.Fatal("Not enough arguments.")
 	}
@@ -31,22 +37,88 @@ func main() {
 	
 	blacklist := []string{"all", "default"}
 
-	S.Path = filepath.Clean(os.Args[1])
-
-	var visit = func(p string, f os.FileInfo, e error) error {
-		if f.IsDir() && p != S.Path {
-			_, file := filepath.Split(p)
-			if !inList(file, blacklist) {
-				S.Sites = append(S.Sites, file)
-			}
-		}
-		return nil
-	}
-	if e := filepath.Walk(S.Path, visit); e != nil {
+	S.Path, e = os.Getwd()
+	if e != nil {
 		log.Fatal(e)
 	}
 
-	for _, site := range S.Sites {
-		fmt.Println(site)
+	module := os.Args[1]
+
+	// var visit = func(p string, f os.FileInfo, e error) error {
+	// 	if f.IsDir() && p != S.Path {
+	// 		_, file := filepath.Split(p)
+	// 		if !inList(file, blacklist) {
+	// 			S.Sites = append(S.Sites, file)
+	// 		}
+	// 	}
+	// 	return nil
+	// }
+	// if e := filepath.Walk(S.Path, visit); e != nil {
+	// 	log.Fatal(e)
+	// }
+
+	// Get all file paths in the current working directory.
+	files, e := filepath.Glob(fmt.Sprintf("%s/*", S.Path))
+	if e != nil {
+		log.Fatal(e)
 	}
+
+	// Loop over files and append only directories, that are not blacklisted, to S.Sites.
+	for _, file := range files {
+		_, f := filepath.Split(file)
+
+		finfo, e := os.Lstat(file)
+		if e != nil {
+			log.Fatal(e)
+		}
+
+		if finfo.IsDir() && !inList(f, blacklist) {
+			S.Sites = append(S.Sites, f)
+		}
+	}
+
+	var tcount, encount, dcount int
+
+	green := ansi.ColorCode("green+h:black")
+	red := ansi.ColorCode("red+h:black")
+	reset := ansi.ColorCode("reset")
+
+	fmt.Println(S.Path)
+
+	for _, site := range S.Sites {
+		tcount++
+		drushcommand := exec.Command("drush", "-l", site, "pmi", module)
+		grepcommand := exec.Command("grep", "Status")
+		grepcommand.Stdin, _ = drushcommand.StdoutPipe()
+
+		// Create a buffer of bytes.
+		var b bytes.Buffer
+
+		// Assign the address of our buffer to grepcommand.Stdout.
+		grepcommand.Stdout = &b
+
+		// Start grepcommand.
+		_ = grepcommand.Start()
+
+		// Run syscommand
+		_ = drushcommand.Run()
+
+		// Wait for grepcommand to exit.
+		_ = grepcommand.Wait()
+
+		s := fmt.Sprintf("%s", &b)
+
+		if strings.Contains(s, "enabled") {
+			encount++
+			fmt.Printf("%sModule %s is enabled on %s.%s\n", green, module, site, reset)
+		} else {
+			dcount++
+			fmt.Printf("%sModule %s is not enabled on %s.%s\n", red, module, site, reset)
+		}
+	}
+
+	fmt.Printf("\n\nModule: %s", module)
+	fmt.Printf("Total number of sites: %d\n", tcount)
+	fmt.Printf("%s%s is enabled on %d sites.%s\n", green, module, encount, reset)
+	fmt.Printf("%s%s is disabled on %d sites.%s\n", red, module, dcount, reset)
 }
